bfb255ed091165018766ed8ed14bb8c816b982f7
Vlad Sitnikov <probjlad@mail.ru>
2021-06-30 19:19:10 +0300

    Hello world

--- praktika1/Form1.cs
+++ praktika1/Form1.cs
@@ -16,5 +16,10 @@ namespace praktika1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello world");
+        }
     }
 }
